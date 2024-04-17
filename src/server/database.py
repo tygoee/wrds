7a512edf5d961c809b13a01acfa347323b0b3628
tygoee <tygoee@outlook.com>
2024-04-17 12:35:27 +0200

    Updated wrong year

--- src/server/database.py
+++ src/server/database.py
@@ -1,5 +1,5 @@
 # WRDS: Learning words online
-# Copyright (C) 2023  Tygo Everts
+# Copyright (C) 2024  Tygo Everts
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
